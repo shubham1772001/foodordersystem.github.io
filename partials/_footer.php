6bd5fda61babf614821214ef74e3519c0b3eb340
shubham1772001 <shubhambhardwaj482001@gmail.com>
2024-03-13 17:23:29 +0530

    add

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2024 Designed by <a href="mailto:shubhambharadwaj482001@gmail.com;" target="_blank" rel="noopener noreferrer">@shubhambharadwaj</a></p>
+</div>
